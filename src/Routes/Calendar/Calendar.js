32968244a89c555837a973d1cad7f6c06f654863
Mero <mero@Meros-iMac-Pro.local>
2020-07-10 11:03:17 -0230

    created calendar files

---
+++ src/Routes/Calendar/Calendar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from "styled-components";
+
+class Calendar extends Component {
+    
+    date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+    render() {
+        return (
+          <div > 
+          </div>
+        );
+      }
+}
+
+export default Calendar;
