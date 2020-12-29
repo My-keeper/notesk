009298cb532e07083072a7a124ac451a7d13005f
Mero <mero@Meros-iMac-Pro.local>
2020-12-29 13:35:38 -0330

    finishing desiging the page

---
+++ src/Routes/Signup/Signup.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Nav from "../../UI/NavBar";
+import LoginForm from "./LoginForm";
+import BlurDiv from "../../UI/BlurDiv";
+
+const Login = (props) => {
+  return (
+    <div>
+      <BlurDiv blurDegree={"1.4px"}/>
+      <Nav ColorChanged={props.colorchanged} RecieveColor={props.Color}/>
+      <LoginForm ColorChanged={props.colorchanged} RecieveColor={props.Color}/>
+    </div>
+  );
+};
+
+export default Login;
