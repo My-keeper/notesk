3394ba4121b80529a0319986b03cc26d9f394fb4
Mero <mero@Meros-iMac-Pro.local>
2021-01-16 16:07:12 -0330

    finxed the map now I need to work on the targeted value

--- src/Routes/Calendar/test.js
+++ src/Routes/Calendar/test.js
@@ -23,29 +23,31 @@ const Test = (props) => {
     
     const Time = [<HourValue/>, <MinutsValue/>, <TwelveHourSystemValue/>]
     // console.log(ChosenHour)   
-    return <FadeIn><SelectData 
-            PlaceholderValue={props.PHValue}
-            inputType={"text"}
-            name="title"
-            widthValue={"105px"}
-            paddingVale={"4px"}
-            outlineValue={"none"}
-            fontSizeValue={"1.2em"}
-            fontFamilyValue={"inherit"}
-            IsCalledValue={"FOCUS"} 
-            borderRadiusValue= {"7px"}
-            backGroundColorValue={ props.RecievedColor.UserInputBGC}
-            FontColorValue={props.RecievedColor.UserInputFC}
-            PlaceHolderColorValue={props.RecievedColor.UserInputPHC}
-            borderColorValue={props.RecievedColor.BorderColor} 
-            BorderValue={"solid"}
-            borderWidthValue={"thin"}
-            marginRightValue={"15px"}
-            onchangeValue = {handle}
-            InputValue = {ChosenHour}
+    return Time.map((Inputvalue) => { 
+        return <FadeIn><SelectData 
+        PlaceholderValue={props.PHValue}
+        inputType={"text"}
+        name="title"
+        widthValue={"105px"}
+        paddingVale={"4px"}
+        outlineValue={"none"}
+        fontSizeValue={"1.2em"}
+        fontFamilyValue={"inherit"}
+        IsCalledValue={"FOCUS"} 
+        borderRadiusValue= {"7px"}
+        backGroundColorValue={ props.RecievedColor.UserInputBGC}
+        FontColorValue={props.RecievedColor.UserInputFC}
+        PlaceHolderColorValue={props.RecievedColor.UserInputPHC}
+        borderColorValue={props.RecievedColor.BorderColor} 
+        BorderValue={"solid"}
+        borderWidthValue={"thin"}
+        marginRightValue={"15px"}
+        onchangeValue = {handle}
+        InputValue = {ChosenHour}
         >
-            {<MinutsValue/>}
+            {Inputvalue}
         </SelectData></FadeIn>
+    })
 }
 
 export default Test;
