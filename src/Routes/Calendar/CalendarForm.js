757d8fd69652730659af9243ed0d6781e446f592
Mero <mero@Meros-iMac-Pro.local>
2021-02-02 07:47:55 -0330

    add the schedule form to the new organized Calendar

--- src/Routes/Calendar/CalendarForm.js
+++ src/Routes/Calendar/CalendarForm.js
@@ -5,78 +5,88 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { INITIAL_EVENTS, createEventId } from './event-utils'
-
+import styled from 'styled-components';
+const CalenderContainer = styled.div`
+    margin: 10px;
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    min-height: 100%;
+    font-size: 16px;
+    color: cornflowerblue 
+    
+`
 export default class DemoApp extends React.Component {
 
   state = {
     weekendsVisible: true,
     currentEvents: []
   }
-
+   
   render() {
-    return (
-      <div className='demo-app'>
-        {this.renderSidebar()}
-        <div className='demo-app-main'>
+    return ( 
+        <CalenderContainer  >
           <FullCalendar
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
             headerToolbar={{
               left: 'prev,next today',
               center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay, listPlugin'
+              right: 'dayGridMonth,timeGridWeek,timeGridDay, list'
             }}
             initialView='dayGridMonth'
-            editable={true}
-            selectable={true}
+            titleFormat= {{ month: 'short',year: 'numeric',day: 'numeric'}} //this is for the tile what should be there for the user
+            height="850px"
+            contentHeight="800px"
+            handleWindowResize="true"
+            locale= 'En' //this is for the languages option 
+            timeZone= 'canada/nl' //to get the time zone of your location that is why we will be using the location in the sigup or make the browser detecte it
+            editable={true} //to edit the info
+            selectable={true} //to enable selection
             selectMirror={true}
             dayMaxEvents={true}
             weekends={this.state.weekendsVisible}
+            eventDisplay= 'block' //this is used to high light the event that are created 
+            eventTextColor="black" //this is for the styling of the text for each event
+            eventBackgroundColor="cornflowerblue" //This is for the background of each event
+            eventBorderColor="pink" //The border color
             initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
             select={this.handleDateSelect}
             eventContent={renderEventContent} // custom render function
             eventClick={this.handleEventClick}
             eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-            /* you can update a remote database when these fire:
-            eventAdd={function(){}}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
           />
-        </div>
-      </div>
+        </CalenderContainer> 
     )
   }
 
-  renderSidebar() {
-    return (
-      <div className='demo-app-sidebar'>
-        <div className='demo-app-sidebar-section'>
-          <h2>Instructions</h2>
-          <ul>
-            <li>Select dates and you will be prompted to create a new event</li>
-            <li>Drag, drop, and resize events</li>
-            <li>Click an event to delete it</li>
-          </ul>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <label>
-            <input
-              type='checkbox'
-              checked={this.state.weekendsVisible}
-              onChange={this.handleWeekendsToggle}
-            ></input>
-            toggle weekends
-          </label>
-        </div>
-        <div className='demo-app-sidebar-section'>
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>
-            {this.state.currentEvents.map(renderSidebarEvent)}
-          </ul>
-        </div>
-      </div>
-    )
-  }
+  // renderSidebar() {
+  //   return (
+  //     <div className='demo-app-sidebar'>
+  //       <div className='demo-app-sidebar-section'>
+  //         <h2>Instructions</h2>
+  //         <ul>
+  //           <li>Select dates and you will be prompted to create a new event</li>
+  //           <li>Drag, drop, and resize events</li>
+  //           <li>Click an event to delete it</li>
+  //         </ul>
+  //       </div>
+  //       <div className='demo-app-sidebar-section'>
+  //         <label>
+  //           <input
+  //             type='checkbox'
+  //             checked={this.state.weekendsVisible}
+  //             onChange={this.handleWeekendsToggle}
+  //           ></input>
+  //           toggle weekends
+  //         </label>
+  //       </div>
+  //       <div className='demo-app-sidebar-section'>
+  //         <h2>All Events ({this.state.currentEvents.length})</h2>
+  //         <ul>
+  //           {this.state.currentEvents.map(renderSidebarEvent)}
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   handleWeekendsToggle = () => {
     this.setState({
