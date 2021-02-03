dc234a086bc76b06e90a93e2f11279216c3b3fb6
Mero <arsmohamed@outlook.com>
2021-02-02 21:22:26 -0330

    changed class component to functional component

--- src/Routes/Calendar/CalendarForm.js
+++ src/Routes/Calendar/CalendarForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState }from 'react'
 import FullCalendar, { formatDate } from '@fullcalendar/react'
 import listPlugin from '@fullcalendar/list';
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -14,92 +14,21 @@ const CalenderContainer = styled.div`
     color: cornflowerblue 
     
 `
-export default class DemoApp extends React.Component {
+const CalendarForm = (props) => {
 
-  state = {
-    weekendsVisible: true,
-    currentEvents: []
-  }
+  const weekendsVisible = true
+  const [currentEvents, ChangecurrentEvents]= useState([])
+  const  [DataClicked, ChangeDataClicked] = useState(false)
    
-  render() {
-    return ( 
-        <CalenderContainer  >
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay, list'
-            }}
-            initialView='dayGridMonth'
-            titleFormat= {{ month: 'short',year: 'numeric',day: 'numeric'}} //this is for the tile what should be there for the user
-            height="850px"
-            contentHeight="800px"
-            handleWindowResize="true"
-            locale= 'En' //this is for the languages option 
-            timeZone= 'canada/nl' //to get the time zone of your location that is why we will be using the location in the sigup or make the browser detecte it
-            editable={true} //to edit the info
-            selectable={true} //to enable selection
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={this.state.weekendsVisible}
-            eventDisplay= 'block' //this is used to high light the event that are created 
-            eventTextColor="black" //this is for the styling of the text for each event
-            eventBackgroundColor="cornflowerblue" //This is for the background of each event
-            eventBorderColor="pink" //The border color
-            initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-            select={this.handleDateSelect}
-            eventContent={renderEventContent} // custom render function
-            eventClick={this.handleEventClick}
-            eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-          />
-        </CalenderContainer> 
-    )
-  }
-
-  // renderSidebar() {
-  //   return (
-  //     <div className='demo-app-sidebar'>
-  //       <div className='demo-app-sidebar-section'>
-  //         <h2>Instructions</h2>
-  //         <ul>
-  //           <li>Select dates and you will be prompted to create a new event</li>
-  //           <li>Drag, drop, and resize events</li>
-  //           <li>Click an event to delete it</li>
-  //         </ul>
-  //       </div>
-  //       <div className='demo-app-sidebar-section'>
-  //         <label>
-  //           <input
-  //             type='checkbox'
-  //             checked={this.state.weekendsVisible}
-  //             onChange={this.handleWeekendsToggle}
-  //           ></input>
-  //           toggle weekends
-  //         </label>
-  //       </div>
-  //       <div className='demo-app-sidebar-section'>
-  //         <h2>All Events ({this.state.currentEvents.length})</h2>
-  //         <ul>
-  //           {this.state.currentEvents.map(renderSidebarEvent)}
-  //         </ul>
-  //       </div>
-  //     </div>
-  //   )
-  // }
-
-  handleWeekendsToggle = () => {
-    this.setState({
-      weekendsVisible: !this.state.weekendsVisible
-    })
-  }
-
-  handleDateSelect = (selectInfo) => {
+  const handleDateSelect = (selectInfo) => {
     let title = prompt('Please enter a new title for your event')
     let calendarApi = selectInfo.view.calendar
 
     calendarApi.unselect() // clear date selection
-
+    console.log(selectInfo.startStr,
+      selectInfo.endStr,
+       selectInfo.allDay
+       )
     if (title) {
       calendarApi.addEvent({
         id: createEventId(),
@@ -111,34 +40,79 @@ export default class DemoApp extends React.Component {
     }
   }
 
-  handleEventClick = (clickInfo) => {
-    if (alert(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove()
-    }
-  }
-
-  handleEvents = (events) => {
-    this.setState({
-      currentEvents: events
-    })
-  }
-
-}
-
-function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
+  return ( 
+    <CalenderContainer  >
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay, list'
+        }}
+        initialView='dayGridMonth'
+        titleFormat= {{ month: 'short',year: 'numeric',day: 'numeric'}} //this is for the tile what should be there for the user
+        height="850px"
+        contentHeight="800px"
+        handleWindowResize="true"
+        locale= 'En' //this is for the languages option 
+        timeZone= 'canada/nl' //to get the time zone of your location that is why we will be using the location in the sigup or make the browser detecte it
+        editable={true} //to edit the info
+        selectable={true} //to enable selection
+        selectMirror={true}
+        dayMaxEvents={true}
+        weekends={weekendsVisible}
+        eventDisplay= 'block' //this is used to high light the event that are created 
+        eventTextColor="black" //this is for the styling of the text for each event
+        eventBackgroundColor="cornflowerblue" //This is for the background of each event
+        eventBorderColor="pink" //The border color
+        initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+        select={handleDateSelect}
+        // eventContent={renderEventContent} // custom render function
+        // eventClick={handleEventClick}
+        // eventsSet={handleEvents} // called after events are initialized/added/changed/removed
+      />
+    </CalenderContainer> 
   )
-}
+      }
+export default CalendarForm
 
-function renderSidebarEvent(event) {
-  return (
-    <li key={event.id}>
-      <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
-      <i>{event.title}</i>
-    </li>
-  )
-}
+  // handleWeekendsToggle = () => {
+  //   setState({
+  //     weekendsVisible: !state.weekendsVisible
+  //   })
+  // }
+
+  
+
+  // handleEventClick = (clickInfo) => {
+  //   if (alert(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+  //     clickInfo.event.remove()
+  //   }
+  // }
+
+//   handleEvents = (events) => {
+//     setState({
+//       currentEvents: events
+//     })
+//   }
+
+// }
+
+// function renderEventContent(eventInfo) {
+//   return (
+//     <>
+//       <b>{eventInfo.timeText}</b>
+//       <i>{eventInfo.event.title}</i>
+//     </>
+//   )
+// }
+
+// function renderSidebarEvent(event) {
+//   return (
+//     <li key={event.id}>
+//       <b>{formatDate(event.start, {year: 'numeric', month: 'short', day: 'numeric'})}</b>
+//       <i>{event.title}</i>
+//     </li>
+//   )
+// }
+
