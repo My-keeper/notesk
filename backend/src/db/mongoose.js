5f3bf8fcc4ab737d9da9405fc041f1fe0b6ae5e1
Mohamed <Amr.Mohamed@windriver.com>
2021-02-19 11:40:19 -0500

    started the DataBase

--- backend/src/db/mongoose.js
+++ backend/src/db/mongoose.js
@@ -1,1 +1,29 @@
-const mongoose = require('mongodb')
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/notask-Test', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'mero',
+    age: '23'
+})
+
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  }); 
