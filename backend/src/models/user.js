05a5896c0a4d1374a3820921c821062685a3ee99
Mero <mero@Meros-iMac-Pro.local>
2021-02-25 08:27:13 -0330

    Backend Login rout problems

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,13 +1,16 @@
-// /*
-//     this area so we create the models for each route that will be 
-//     called in the server so the info will get access in the server route
-// */
+/*
+    this area so we create the models for each route that will be 
+    called in the server so the info will get access in the server route
+
+    The second argument of the mongoose model is turned to be the schema that 
+    medware uses to for the backend 
+*/
  
 const mongoose = require('mongoose')
 const validator = require('validator') 
+const bcrypt = require('bcrypt');
 
-
-const User = mongoose.model('Users', {
+const userSchema = new mongoose.Schema({
     firstName: {
     type: String, 
     required: true,
@@ -24,6 +27,7 @@ const User = mongoose.model('Users', {
     //     required: true,
     // },
     email: {
+        unique: true, //to make sure there is no repeated email
         type: String, 
         required: true,
         lowercase: true,
@@ -76,5 +80,43 @@ const User = mongoose.model('Users', {
         }
     }
 })
+
+//Created a findByCredentials so we can check if the user loggedin with the same credential that we have
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    console.log("we are here ")
+
+    if (user) {
+        console.log("it is the email")
+        throw new Error('Unable to login')
+    }
+    
+    //The first argument is the text password and the second is the hashed one to compare them 
+    const isMatch = await bcrypt.compare( password, user.password)
+    
+    if (!isMatch) {
+        console.log("it is the password")
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+/*
+    we are using function here becasue arrow function dont bind 
+    We call next after we are done in the function and what it needs to do 
+*/
+userSchema.pre('save', async function (next) {
+    //this give us acces to the user that is about to be saved 
+    const user = this
+    
+    //Here we will be hashing the password
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+const User = mongoose.model('Users', userSchema)
 
 module.exports = User
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
 
+//SignUp
 router.post("/Users", async (req, res) => {
   const user = new User(req.body); //take the info from the website and then save it in user
   //saving the info to the database and see if will match the info in the model or not
@@ -12,6 +13,17 @@ router.post("/Users", async (req, res) => {
       res.status(400).send(e)
   } 
 });
+//SignIn
+router.post("/Users/login", async (Req, res) => {
+  try{
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    res.send(user)
+  } catch (e) {
+    res.status(400).send()
+  }
+
+})
+
 
 router.get("/Users", async (req, res) => {
   //we can add the name of the User we are looking for inside the {}
@@ -22,5 +34,14 @@ router.get("/Users", async (req, res) => {
       res.status(500).send(e)
   } 
 });
+// we are missing finding user using the email and updaing user if needed and deleteing user 
+//there is  a prob with meddlewear for fixing watch video 104 at 9 mins 
+
+//for updating 
+// router.patch("/Users/:email", async (Req, res) => {
+//   const updates = Object.keys
+  
+  
+// })
 
 module.exports = router
